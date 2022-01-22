920ad5114855883c98bcce1892b7a148c8fc0b80
Laura OBrien <lauraobdev@gmail.com>
2022-01-22 08:36:05 -0500

    made all-in-one function

--- calc/app.py
+++ calc/app.py
@@ -3,3 +3,19 @@ from flask import Flask, request
 from operations import add, sub, mult, div
 
 app = Flask(__name__)
+
+OPERATION = {
+    "add": add,
+    "sub": sub,
+    "div": div,
+    "mult": mult
+}
+
+@app.route("math/<operations>")
+def test_all_in_one(operations):
+    
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = OPERATION[operations](a,b)
+
+
