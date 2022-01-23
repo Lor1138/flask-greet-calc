9075fd55dadd98fef6f1a06567349982b43ef0f7
Laura OBrien <lauraobdev@gmail.com>
2022-01-23 07:51:06 -0500

    changed dict and query parameter names

--- calc/app.py
+++ calc/app.py
@@ -4,18 +4,20 @@ from operations import add, sub, mult, div
 
 app = Flask(__name__)
 
-OPERATION = {
+OPERATIONs = {
     "add": add,
     "sub": sub,
     "div": div,
     "mult": mult
 }
 
-@app.route("math/<operations>")
-def test_all_in_one(operations):
+@app.route("math/<operation>")
+def test_all_in_one(operation):
     
     a = int(request.args.get("a"))
     b = int(request.args.get("b"))
-    result = OPERATION[operations](a,b)
+    result = OPERATION[operation](a,b)
+
+    return str(result)
 
 
